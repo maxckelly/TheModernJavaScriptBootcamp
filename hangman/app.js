10bfb89329a43ce9b5bc80efb846fd1b1d694ed8
Max Kelly <max@revelthat.com>
2019-07-29 18:22:08 +1000

    106. Exploring Another API

--- hangman/app.js
+++ hangman/app.js
@@ -1,4 +1,6 @@
-
+// HTTP (Hypertext Transfer Protocol)
+  // Request - What we want to do 
+  // Response - What was actually done 
 const puzzleEl = document.querySelector("#puzzle"); // Selects the id element in html file.
 const statusEl = document.querySelector('#status')
 const game1 = new Hangman("Cat Parts", 2);
@@ -6,15 +8,42 @@ const game1 = new Hangman("Cat Parts", 2);
 puzzleEl.textContent = game1.puzzle;
 statusEl.textContent = game1.statusMessage
 
-window.addEventListener("keypress", function(e) {
+window.addEventListener("keypress", (e) => {
   const guess = String.fromCharCode(e.charCode);
   game1.makeGuess(guess);
   puzzleEl.textContent = game1.puzzle;
   statusEl.textContent = game1.statusMessage
 });
 
+// Making an HTTP request 
+const request = new XMLHttpRequest()
 
+request.addEventListener('readystatechange', (e) => {
+  if(e.target.readyState === 4 && e.target.status === 200) {
+    const data = JSON.parse(e.target.responseText)
+    console.log(data);
+  } else if (e.target.readyState === 4) {
+    console.log('An error has taken place')
+  }
+})
 
+request.open('GET',`http://puzzle.mead.io/puzzle`)
+request.send()
+
+// The below pulls data from restcountries url and then displays the countryCode name in the console. 
+const countryCode = "US";
+const countryRequest = new XMLHttpRequest() 
+
+countryRequest.addEventListener('readystatechange', (e) => {
+  if(e.target.readyState === 4 && e.target.status === 200) {
+    const data = JSON.parse(e.target.responseText)
+    const country = data.find((country) => country.alpha2Code === countryCode) // alpha2Code is the request that is in the array.
+    console.log(country.name) // This prints the country name 
+  } 
+})
+
+countryRequest.open('GET', `https://restcountries.eu/rest/v2/all`) // This is the URL that we request the countries from.
+countryRequest.send()
 //----- Primitive value: string, number, boolean, null, undefined -----
 
 // Object: myObject --> product --> Object.prototype --> null // This is how the below code will run it will go through product if nothing there it then goes through object.prototype if nothing there it returns undefined
