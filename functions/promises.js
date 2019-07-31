c0de74e4bcbbd86159aee4201115681184bc515b
Max Kelly <max@revelthat.com>
2019-07-31 17:30:38 +1000

    113. Promise Chaining

--- functions/promises.js
+++ functions/promises.js
@@ -1,16 +1,26 @@
 // Callback
 // The below is one way of doing this instead of promise, this is called a callBack
-const getDataCallBack = (callBack) => {
+const getDataCallback = (num, callback) => {
     setTimeout(() => {
-        callBack(undefined, 'This is the data')
-    }, 2000);
+        if (typeof num === 'number') {
+            callback(undefined, num * 2)
+        } else {
+            callback('Number must be provided')
+        }
+    }, 2000)
 }
 
-getDataCallBack((err, data) => {
+getDataCallback(2, (err, data) => {
     if (err) {
         console.log(err)
     } else {
-        console.log(data)
+        getDataCallback(data, (err, data) => {
+            if (err) {
+                console.log('err')
+            } else {
+                console.log(data)
+            }
+        })
     } 
 })
 
@@ -18,23 +28,30 @@ getDataCallBack((err, data) => {
 // Promises expect a single argument. The promise function is a function to handle errors. 
 // The benefits for using the promise function is that its clear and straight to the point.
 // Its impossible to run more than just one of these functions. You cannot resolve twice, reject twice or resolve and reject.
-const getDataPromise = (data) => new Promise((resolve, reject) => {
+const getDataPromise = (num) => new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve(`This is my success data: ${data}`)
-        //reject("This is my promise error");
+        typeof num === 'number' ? resolve(num * 2) : reject('Number must be provided')
       }, 2000);
 })
 
-const myPromise = getDataPromise()
-
-myPromise.then((data) => {
-    console.log(data) 
+getDataPromise(2).then((data) => {
+    getDataPromise(data).then((data) => {
+        console.log(`Promise data: ${data}`)
+    }, (err) => {
+        console.log(err)
+    })
 }, (err) => {
     console.log(err)
 })
 
-myPromise.then((data) => {
-    console.log(data) 
-}, (err) => {
+// Promise Chaining 
+
+getDataPromise(10).then((data) => {
+    return getDataPromise(data) 
+}).then((data) => {
+    return getDataPromise(data)
+}).then((data) => {
+    console.log(data)
+}).catch((err) => {
     console.log(err)
 })
