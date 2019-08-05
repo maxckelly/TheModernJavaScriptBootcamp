b5a0efa933d160e049539352175e3a80745d8246
Max Kelly <max@revelthat.com>
2019-08-05 16:25:43 +1000

    117. Async/Await

---
+++ functions/async-await.js
@@ -0,0 +1,26 @@
+
+// note: Async's always return a promise so Promise { 12 }
+
+const getDataPromise = (num) => new Promise((resolve, reject) => {
+    setTimeout(() => {
+        typeof num === 'number' ? resolve(num * 2) : reject('Number must be provided')
+      }, 2000);
+})
+
+const processData = async () => {
+    let data = await getDataPromise(2) 
+    data = await getDataPromise(data)
+    data = await getDataPromise(data);
+    return data
+    // The code above does what the code below does but less words.
+
+    // getDataPromise(2).then((data) => {
+    //     console.log(data)
+    // })
+}
+
+processData().then((data) => {
+    console.log('Data', data)
+}).catch((error) => {
+    console.log('Error', error)
+})
--- hangman/requests.js
+++ hangman/requests.js
@@ -3,7 +3,19 @@
   // Response - What was actually done 
 
 //-----FETCHING PUZZLE API -----
-const getPuzzle = (wordCount) => {
+const getPuzzle = async (wordCount) => {
+    const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+    
+    if (response.status === 200) {
+        const data = await response.json()
+        return data.puzzle
+    } else {
+        throw new Error('Unable to get puzzle')
+    }
+  
+}
+
+const getPuzzleOld = (wordCount) => {
     return fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`).then((response) => {
         if (response.status === 200) {
             return response.json()
