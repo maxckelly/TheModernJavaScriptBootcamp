66be817c266c8dd2788009ee6a22a56273fce7b6
Max Kelly <max@revelthat.com>
2019-08-08 17:10:03 +1000

    135. JavaScript Modules: Part I

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -1,11 +1,17 @@
-const name = 'Jen'
+// Import statement - This is one way to export files across.
+//import './utilities.js';
+
+// The below import is grabbing code from the utilities file. To grab multiple things just separate with a ','
+import { add, name } from "./utilities.js";
+import { scream } from "./scream.js";
+
+console.log(add(32, 31))
+console.log('index.js')
 console.log(name)
+console.log(scream(name))
 
-class Hangman {
-    myMethod() {
-        return 'Testing'
-    }
-}
-
-const hangman = new Hangman()
-console.log(hangman.myMethod())
+// 1. Create new file called scream.js
+// 2. Export a "scream" function that takes a string 
+//  a. Convert that string to upper case
+//  b. Add ! onto the end
+// 3. Import scream into index.js and use it. 
---
+++ boilerplate/src/utilities.js
@@ -0,0 +1,5 @@
+
+console.log('utilities.js')
+// Export statement - This is a way to export functions, const etc.. across files
+export const add = (a, b) => a + b
+export const name = 'Andrew'
